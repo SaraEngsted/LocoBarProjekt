0c8d2dc1268520081e81e2f9b21d3a4738363116
naom0649 <naom0649@stud.kea.dk>
2023-06-15 14:24:12 +0200

    lavet en ny main activity 2

--- app/src/main/java/com/example/locobar/MainActivity.java
+++ app/src/main/java/com/example/locobar/MainActivity.java
@@ -2,27 +2,34 @@ package com.example.locobar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
-
-    public void button1(View view){
-
-        Log.i("info","Button pressed! 1");
-    }
-
-
-    public void button2(View view){
-
-        Log.i("info","Button pressed! 1");
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+
+            }
+
+        });
+
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 
 
