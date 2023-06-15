d421ba507423cd078e4ad4f52bb6225a80a0af01
naom0649 <naom0649@stud.kea.dk>
2023-06-15 12:39:10 +0200

    buttom1

--- app/src/main/java/com/example/locobar/MainActivity.java
+++ app/src/main/java/com/example/locobar/MainActivity.java
@@ -3,9 +3,21 @@ package com.example.locobar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public void button(View view){
+
+        Log.i("info","Button pressed! 1");
+    }
+
+    public void button2(View view){
+
+        Log.i("info","Button pressed! 1");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
