18db2f4833b8f806b7a96c4936f68ad96e8c0646
SaraEngsted <sara.andreasen@live.dk>
2023-06-21 18:52:27 +0200

    firebase

--- app/src/main/java/com/example/locobar/MainActivity.java
+++ app/src/main/java/com/example/locobar/MainActivity.java
@@ -19,24 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivity2();
-
-            }
-
-        });
+        button.setOnClickListener(view -> openActivity2());
 
         button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivity3();
-
-            }
-
-        });
+        button2.setOnClickListener(view -> openActivity3());
 
     }
 
--- app/src/main/java/com/example/locobar/MainActivity3.java
+++ app/src/main/java/com/example/locobar/MainActivity3.java
@@ -1,13 +1,55 @@
 package com.example.locobar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity3 extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity3 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        button = (Button) findViewById(R.id.buttonAdd);
+        button.setOnClickListener(view -> openActivity2());
+
+        button2 = (Button) findViewById(R.id.tilbage1);
+        button2.setOnClickListener(view -> openActivity());
+
+    }
+
+    public void saveInfo (View view){
+
+    }
+
+    public void openActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivity2() {
+        Intent intent2 = new Intent(this, MainActivity3.class);
+        startActivity(intent2);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
